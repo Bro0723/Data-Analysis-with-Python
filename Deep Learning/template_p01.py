0184aaa4819b006352909354fca8cf094b299461
Bro0723 <940909925@qq.com>
2024-11-29 18:27:33 +0300

    Update template_p01.py

--- Deep Learning/template_p01.py
+++ Deep Learning/template_p01.py
@@ -3,7 +3,6 @@ import numpy as np
 def softmax(vector):
     '''
     vector: np.array of shape (n, m)
-    
     return: np.array of shape (n, m)
         Matrix where softmax is computed for every row independently
     '''
@@ -12,6 +11,29 @@ def softmax(vector):
     exp_denominator = np.sum(exp_vector, axis=1)[:, np.newaxis]
     softmax_ = exp_vector / exp_denominator
     return softmax_
+
+def dot_product_attention_score(decoder_hidden_state, encoder_hidden_states):
+    '''
+    decoder_hidden_state: np.array of shape (n_features, 1)
+    encoder_hidden_states: np.array of shape (n_features, n_states)
+    
+    return: np.array of shape (1, n_states)
+        Array with dot product attention scores
+    '''
+    attention_scores = np.dot(decoder_hidden_state.T, encoder_hidden_states)
+    return attention_scores
+
+def dot_product_attention(decoder_hidden_state, encoder_hidden_states):
+    '''
+    decoder_hidden_state: np.array of shape (n_features, 1)
+    encoder_hidden_states: np.array of shape (n_features, n_states)
+    
+    return: np.array of shape (n_features, 1)
+        Final attention vector
+    '''
+    softmax_vector = softmax(dot_product_attention_score(decoder_hidden_state, encoder_hidden_states))
+    attention_vector = softmax_vector.dot(encoder_hidden_states.T).T
+    return attention_vector 
 
 def multiplicative_attention(decoder_hidden_state, encoder_hidden_states, W_mult):
     '''
@@ -22,8 +44,15 @@ def multiplicative_attention(decoder_hidden_state, encoder_hidden_states, W_mult
     return: np.array of shape (n_features_enc, 1)
         Final attention vector
     '''
-    # your code here
+    # 计算 attention score
+    attention_scores = np.dot(W_mult, decoder_hidden_state) + encoder_hidden_states.T
+    attention_scores = np.dot(attention_scores.T, encoder_hidden_states)
     
+    # 计算 softmax 权重
+    softmax_vector = softmax(attention_scores)
+    
+    # 计算加权和
+    attention_vector = softmax_vector.dot(encoder_hidden_states.T).T
     return attention_vector
 
 def additive_attention(decoder_hidden_state, encoder_hidden_states, v_add, W_add_enc, W_add_dec):
@@ -37,6 +66,13 @@ def additive_attention(decoder_hidden_state, encoder_hidden_states, v_add, W_add
     return: np.array of shape (n_features_enc, 1)
         Final attention vector
     '''
-    # your code here
+    # 计算注意力得分
+    scores = v_add.T @ np.tanh(np.dot(W_add_enc, encoder_hidden_states) + np.dot(W_add_dec, decoder_hidden_state))
     
+    # 计算 softmax 权重
+    softmax_vector = softmax(scores)
+    
+    # 计算加权和
+    attention_vector = softmax_vector.dot(encoder_hidden_states.T).T
     return attention_vector
+
